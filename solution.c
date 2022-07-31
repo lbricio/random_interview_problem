85c7a6d825b4771833079fb55708c0e486d2d35b
lbricio <kirowhisky@gmail.com>
2022-07-31 16:43:26 -0300

    first commit

---
+++ solution.c
@@ -0,0 +1,41 @@
+// ----------------------------------------------------------------------------
+// você receberá um dígito inteiro que está contido entre 0 e 9,
+// imprima na saída padrão todos os números de até 4 dígitos que obedecem 
+// as seguintes regras:
+// - todos os dígitos devem ser menores que o dígito recebido
+// - a soma dos dígitos deve ser igual a 21;
+// - os números devem ser separados por vírgulas (ex: 3666,4566,4656,)
+// ----------------------------------------------------------------------------
+
+#include <stdio.h>
+
+#define first_digit (i/1000)
+#define second_digit ((i%1000 - i%100)/100)
+#define third_digit ((i%100 - i%10)/10)
+#define fourth_digit (i%10)
+
+void print_numbers(int number)
+{
+	int i = 0;
+	while(i++ <= 9999)
+	{
+		if ((first_digit <= number)
+		&& (second_digit <= number) 
+		&& (third_digit  <= number) 
+		&& (fourth_digit <= number))
+			if ((first_digit + second_digit + third_digit + fourth_digit) == 21)
+			{
+				if (first_digit == 0) printf("0"); 
+				printf("%i,",i);
+			}
+	}
+}
+
+int main()
+{
+	int number;
+	scanf("%i",&number);
+	print_numbers(number);
+
+	return (0);
+}
